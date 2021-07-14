a273120923bf634f8666b93ce02b765a1112c0b9
Emmanuel Atama <emmanuelatama@outlook.com>
2021-07-14 17:18:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// 1. Create an array with the following last names: Mason, Marcora, Rico, Neves, Ivanov
+const names = ["Mason", "Marcora", "Rico", "Neves", "Ivanov"];
+
+// 2. Have a prompt that adds your last name to the array
+console.log(names);
+const addLastName = prompt ();
+names.push(addLastName);
+
+// 3. Have an output that sorts the names alphabetically
+const sortLastName = names.sort();
+console.log(sortLastName);
